ad7be49cec587989ddb948b853008ea62952c366
Yongwoo Park <yongwoo1.park@lge.com>
2018-10-14 15:12:24 +0900

    add default welcome page

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(require('morgan')('dev'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+    res.send('Node running');
+})
+
+
+app.listen(8080);
